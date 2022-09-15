8ef1ad49fbf6deb2592089b9d2644660d0cac59c
Jacques Etienne <santiagondiaye1@gmail.com>
2022-09-15 12:38:47 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-	char holberton[] = "Holberton";
-	int i = 0;
-
-	while (holberton[i] != '\0')
-	{
-		_putchar(holberton[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
